20398f067c74a01f2fac21e8c8864029d8ec04a3
zeerob440 <zeerob440@tutanota.com>
2026-06-08 20:55:36 -0400

    updated: Global| documentation.

--- rbac1_assn_zr/nostromo_access.py
+++ rbac1_assn_zr/nostromo_access.py
@@ -1,6 +1,17 @@
 import random
 
-# A model of an RBAC program, in cyberspace no one can hear you scream.
+'''A model of an RBAC program; in cyberspace no one can hear you scream.
+
+Proudly Engineered by Zachary Roberts 08 JUN 2026. 
+
+
+    This program follows CIA Triad by:
+    > Ensuring data information is accessible only to authorized users. 
+    > A 'user' cannot access an 'admin' function and visa versa.
+    > Integrity is maintained by the main program executing an if/elif/else structure,
+        if a role is not 'user' or a role is not'admin' it returns 'UNABLE TO COMPUTE', 
+
+'''
 
 # returns this doc string if role == 'admin'
 def adminQuery(): 
@@ -26,7 +37,7 @@ def userQuery():
     '''
     return user_return
 
-
+# 'admin' and 'user are declared as roles in hard coded class instances. 
 class Crew:
     def __init__(self, name, role):
         self.name = name
@@ -43,14 +54,18 @@ admin_science_officer = Crew(name = 'ASH', role = 'admin')
 
 nostromo_compliment: list = [user_warrant_officer, admin_science_officer]
 
-# selects class instance with random.choice
+'''
+selects class instance with random.choice, this becomes the 'logged' user.
+This allows the program to access both functions for the 'admin' and 'user' roles. 
+
+'''
 simulated_logged_user = random.choice(nostromo_compliment)
 
 welcome: str = f'''WELCOME {simulated_logged_user.name}.\n
 INTERFACE 2037 READY FOR INQUIRY
 ________________________________\n'''  
 
-
+# if/elif/else structure that executes the program logic. 
 if simulated_logged_user.role == 'user':
     execute_user_query = userQuery()
     print (welcome)
