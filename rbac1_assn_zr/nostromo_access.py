0f797aef6b30f33e625350ac63123c0041ee1db9
zeerob440 <zeerob440@tutanota.com>
2026-06-08 20:04:56 -0400

    feat: nostromo_access.py: declared welcome doc string to identify user by name.

--- rbac1_assn_zr/nostromo_access.py
+++ rbac1_assn_zr/nostromo_access.py
@@ -2,7 +2,7 @@ import random
 
 # A model of an RBAC program, in cyberspace no one can hear you scream.
 
-
+# returns this doc string if role == 'admin'
 def adminQuery(): 
     admin_return: str ='''
     NOSTROMO REROUTED\n
@@ -16,7 +16,7 @@ def adminQuery():
     '''
     return admin_return
 
-
+# returns this doc string if role == 'user'
 def userQuery():
     user_return: str ='''
     UNABLE TO CLARIFY.\n
@@ -25,6 +25,7 @@ def userQuery():
     SCIENCE OFFICER EYES ONLY.\n
     '''
     return user_return
+
 
 class Crew:
     def __init__(self, name, role):
@@ -38,20 +39,25 @@ user_warrant_officer = Crew(name ='Ripley', role = 'user')
 admin_science_officer = Crew(name = 'Ash', role = 'admin')
 
 
-# simulate logged in user without input
+# simulate logged in user without input, by placing class instances into list.
 
 nostromo_compliment: list = [user_warrant_officer, admin_science_officer]
 
+# selects class instance with random.choice
 simulated_logged_user = random.choice(nostromo_compliment)
 
-    
+welcome: str = f'''WELCOME {simulated_logged_user.name}\n
+INTERFACE 2037 READY FOR INQUIRY
+________________________________\n'''  
 
 
 if simulated_logged_user.role == 'user':
     execute_user_query = userQuery()
+    print (welcome)
     print(execute_user_query)
 elif simulated_logged_user.role == 'admin':
     execute_admin_query = adminQuery()
+    print (welcome)
     print(execute_admin_query)
 else: 
     print('UNABLE TO COMPUTE.\n')
