3a609894b1c1b050da891a1cc6c0e9b61b28e6bd
MU-TH-ER <zeerob440@tutanota.com>
2026-06-15 10:34:26 -0400

    feat: encrypt_decrypt.py| symmetrical encrypt decrypt process successful.

--- module_2/encrypt_decrypt.py
+++ module_2/encrypt_decrypt.py
@@ -5,8 +5,11 @@ from cryptography.fernet import Fernet
 
 # test message to encrypt. 
 tutanota: str = "Hello World"
+# must be encoded into bytes first
+tutanota_bytes = tutanota.encode()
 # generate key
 symmetrical_key = Fernet.generate_key()
+print('Semmetrical Key below:\n')
 print(symmetrical_key)
 
 # declare var to store generated key
@@ -14,10 +17,15 @@ cipher_process = Fernet(symmetrical_key)
 
 print(cipher_process)
 # encrypt 'tutanota'
-encrypted = cipher_process.encrypt(tutanota)
+encrypted = cipher_process.encrypt(tutanota_bytes)
 # confirm cipher text is returned
-print('Print Check: Is cipher text output below?\n')
+print('Print Check: Is cipher text output below: TRUE\n')
 print(encrypted)
+
+print('Test: printed decrypted tutanota below\n')
+
+decrypted = cipher_process.decrypt(encrypted)
+print(decrypted)
 
 
 
