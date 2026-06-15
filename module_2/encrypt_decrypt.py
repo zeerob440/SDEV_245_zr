fab52a2b3a23aaf3a8f001b9a05fbeb70b9eb91b
MU-TH-ER <zeerob440@tutanota.com>
2026-06-15 09:50:24 -0400

    experiment: encrypt_decrypt.py| testing methods and functions in module

--- module_2/encrypt_decrypt.py
+++ module_2/encrypt_decrypt.py
@@ -1,4 +1,12 @@
-from cryptography import fernet
+from cryptography.fernet import Fernet
 
-print (dir(fernet.Fernet))
-print (help(fernet))
+#print (dir(fernet.Fernet))
+#print (help(fernet))
+
+makes_a_key = Fernet.generate_key()
+print(makes_a_key)
+tutanota: str = "Hello World"
+
+#encrypted = encrypt(tutanota)
+
+#print(encrypted)
