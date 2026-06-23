5bb080cd4a912f024240a3702c603968e8ef8da8
zeerob440 <zeerob440@tutanota.com>
2026-06-23 12:29:01 -0400

    update: hash_y_decryption.py| going another direction.

--- module_3/hash_y_encryption.py
+++ module_3/hash_y_encryption.py
@@ -1,7 +1,8 @@
 import hashlib
+import string
 import rsa
-
 '''
+
 SPECS
 X Choose a language (Python, Java, C/C++, or any language of your choice).
 
@@ -9,23 +10,28 @@ Write an app that generates SHA-256 hashes for input strings or files
 
 Write an app that uses a simple substitution cipher (Caesar cipher or similar) to encrypt/decrypt input text
 
-X Use OpenSSL or a tool to simulate a digital signature (sign/verify).
+Use OpenSSL or a tool to simulate a digital signature (sign/verify).
 X Include a short README explaining your code's functionality
 '''
-input_string1: str ='''
-UNSC PRIORITY TRANSMISSION\n
-FROM: SIERRA 117\n
-TO: ADM HOOD\n
-
-To give the Covenant back their bomb.\n'''
-
-input_string1_bytes = input_string1.encode()
+# return user input when cipher is complete
+user_hash_input: str = 'Enter something to be hashed: '
+# encode into bytes
+input_string1_bytes = user_hash_input.encode()
 #print(input_string1_bytes)
+# create hash object
 hash_obj = hashlib.sha256(input_string1_bytes)
 
 hashed_input = hash_obj.digest()
 
 print(hashed_input)
+
+
+all_runes: str = string.ascii_letters
+for rune in string.ascii_letters:
+    print(rune)
+
+
+
 
 
 
