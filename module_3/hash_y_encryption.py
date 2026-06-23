68bfeda49b7f17019a269f0c6a2015d9880ecb98
zeerob440 <zeerob440@tutanota.com>
2026-06-23 15:31:23 -0400

    feat: hash_y_encryption.py| updated documentation, refined code, removed user input for encryption.

--- module_3/hash_y_encryption.py
+++ module_3/hash_y_encryption.py
@@ -12,7 +12,8 @@ Write an app that uses a simple substitution cipher (Caesar cipher or similar) t
 Use OpenSSL or a tool to simulate a digital signature (sign/verify).
 X Include a short README explaining your code's functionality
 '''
-# return user input when cipher is complete
+print('SHA-256 DEMO BELOW\n')
+# SHA-256 DEMO Below
 user_hash_input: str = 'Enter something to be hashed: '
 # encode into bytes
 input_string1_bytes = user_hash_input.encode()
@@ -22,48 +23,57 @@ hash_obj = hashlib.sha256(input_string1_bytes)
 
 hashed_input = hash_obj.digest()
 
-print(hashed_input)
+print(f'this is your hashed input: {hashed_input}\n')
 
 
-caesar_input: str = 'My dogs are named Mia and Rico'
-
+# CREATE SIMPLE CIPHER 
+print('SIMPLE CIPHER DEMO BELOW.\n')
+# create two lists with the same set of 'runes', but the 'runes' are located at different indices. 
 PLAIN_TEXT_RUNES: list = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']    
 CAESAR_RUNES: list = [' ', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
 
+# input to be encrypted
+caesar_input: str = ' my dogs are named Mia and Rico'
 
-while True:
-    if caesar_input.isdigit():
-        print("Spell numbers out.")
-        continue
-    else:
-        caesar_list = list(caesar_input.upper())
-        break
+# Convert input to an all upper case list so input matches PLAIN_TEXT_RUNES and CAESAR_RUNES expectations.
+caesar_list = list(caesar_input.upper())
 
+# declare a list for the encrypted runes to populate
 encrypted_list: list = []
 
+#parse each 'rune' in caesar_list
 for rune in caesar_list:
- cipher_rune = PLAIN_TEXT_RUNES.index(rune)
+ # retrieve index of current 'rune' in PLAIN_TEXT_RUNES
+ cipher_rune: int = PLAIN_TEXT_RUNES.index(rune)
+ # retrieve the 'rune' from CAESAR_RUNES using index of current 'rune' in PLAIN_TEXT_RUNES
+ # append it to encrypted_list
  encrypted_list.append(CAESAR_RUNES[cipher_rune])
 
+# convert encrypted_list elements to string with .join() method
+encrypted_message: str = ''.join(encrypted_list)
 
-
-encrypted_message = ''.join(encrypted_list)
-
-print('Encrypted message below\n')
+print('Encrypted message below.\n')
 print(encrypted_message)
 
+# decryption process
 print('Decrypted message below.\n')
 
+# list where decrypted 'rune' are stored
 decrypted_list: list = []
 
+# parse each rune in encrypted_list
 for rune in encrypted_list:
-   plain_text_rune = CAESAR_RUNES.index(rune)
+   # retrieve index of current 'rune' in CAESAR_RUNES
+   plain_text_rune: int = CAESAR_RUNES.index(rune)
+   # Retrieve the 'rune' from PLAIN_TEXT_RUNES using the index of the current 'rune' in CAESAR_RUNES
+   # append it to the decrypted_list
    decrypted_list.append(PLAIN_TEXT_RUNES[plain_text_rune])
-   decrypted_message = ''.join(decrypted_list)
+   # converts list to string with .join() method
+   decrypted_message: str = ''.join(decrypted_list)
 
 print(decrypted_message)
 
-    
+# DIGITAL SIGNATURE DEMO BELOW
 
 
 
