f6de1d284c7b7e3a468ca8bf4e9566052eeff2f2
MU-TH-ER <zeerob440@tutanota.com>
2026-06-29 08:26:12 -0400

    feat: mid_term/secure_app.py| directory and application declared.

---
+++ mid_term/secure_app.py
@@ -0,0 +1,13 @@
+'''
+SPECS
+In this project, students will create a small application or script that:
+
+    Accepts user input (e.g., a message or file)
+    Hashes the input using SHA-256 to ensure integrity
+    Encrypts the input using symmetric encryption (e.g., AES)
+    Decrypts the content and verifies its integrity via hash comparison
+2. Students must also:
+
+Write a short explanation describing how their solution upholds confidentiality, integrity, and availability
+Explain the role of entropy and key generation in their implementation
+'''
