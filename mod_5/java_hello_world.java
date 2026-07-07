30eecb32ddbc13d8d8f5984ca98b4686e8d5187d
MU-TH-ER <zeerob440@tutanota.com>
2026-07-07 09:52:14 -0400

    experiment: java_hello_world.java| compiled code, did not return a .exe in the local directory like C++ would? Did it compile?

--- mod_5/java_hello_world.java
+++ mod_5/java_hello_world.java
@@ -1,5 +1,10 @@
 package mod_5;
-
+// first ever Java program.
 public class java_hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello, MU-TH-ER!");
+    }
     
 }
+
+
