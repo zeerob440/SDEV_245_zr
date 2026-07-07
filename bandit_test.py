435721424e65f1994a3da03eb69fe79ca92cf731
zeerob440 <zeerob440@tutanota.com>
2026-07-06 20:02:06 -0400

    feat: bandit_test.py| most insecure program ever completed. Bandit extension linter hates hardcoded password 'guest'

--- bandit_test.py
+++ bandit_test.py
@@ -1,3 +1,14 @@
 
 password: str = 'guest'
 
+def beholdMyStuff():
+    return 'Behold... my stuff.'
+
+print(password)
+
+if password == 'guest':
+    print("pls don't steal my stuff")
+    print(beholdMyStuff())
+else:
+    print(beholdMyStuff())
+    
