fae52f369f8e6a57f8ecaf0c41f747cf1246ed31
MU-TH-ER <zeerob440@tutanota.com>
2026-07-08 16:07:26 -0400

    feat: mod_5/test_programs/injection6.py| added user input sanitation.

--- mod_5/test_programs/injection6.py
+++ mod_5/test_programs/injection6.py
@@ -1,6 +1,15 @@
 import sqlite3
 
-user_id = input('enter id')
+
+# sanatize user input. 
+while True:
+    user_id = input('enter id: ')
+    if user_id.isdigit():
+        user_id = int(user_id)
+        break
+    else:
+        print('ENTER A DIGIT!\n')
+        
 
 connect_db = sqlite3.connect('mod_5/test_programs/the_database.db')
 db_cursor = connect_db.cursor()
