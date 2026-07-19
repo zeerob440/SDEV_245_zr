9af54eab257ed0c57694d3bba2526cdbd22ea4fb
zeerob440 <zeerob440@tutanota.com>
2026-07-19 12:28:21 -0400

    feat: scanner.py: opens and reads files as expected.

--- secret_scanner/scanner.py
+++ secret_scanner/scanner.py
@@ -1,4 +1,19 @@
+import os
 # scanner.py reads cli args
 
 def scan(path):
+    # determine if path is directory or file.
+    is_path_valid = os.path.exists(path)
+    is_file =os.path.isfile(path)
+
+    # print checks
     print(path)
+    print(is_path_valid)
+    print(is_file)
+
+    # validate if path and file are valid. 
+    if is_path_valid == True and is_file == True:
+        with open(path, 'r') as file:
+            print(file.read())
+
+
